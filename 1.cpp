9ef137fe12b90d853bc4d6cfa3073574967f05eb
kuthalRitvik <2023mcb1219@iitrpr.ac.in>
2025-07-15 14:12:54 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	  cout<<"HELLo sublime"<<endl;
+	  int t;
+	  cin>>t;
+	  while(t--){
+	  int n;
+	  cin>>n;
+	  int val=0;
+	  for(int i=0;i<=n;i++){
+	  	  val=val+i;
+	  }
+	  cout<<val<<endl;
+	}
+}
